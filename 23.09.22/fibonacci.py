f83d6e9589ae7a630f02965c8b9254b216d0ce9b
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-09-23 03:14:54 +0400

    Update fibonacci.py

--- 23.09.22/fibonacci.py
+++ 23.09.22/fibonacci.py
@@ -1,1 +1,13 @@
-
+def fib(n):
+    cache = [0] * (n + 1)
+    if n < 0:
+        return (f"You can not enter a negative number!")
+    elif n <= 1:
+       return n
+    if cache[n] != 0:
+       return cache[n]
+    else:
+       cache[n] = fib(n -1) +  fib(n -2)
+       return cache[n]
+n = int(input("Enter a number:"))
+print(fib(n))
