3110cd27d450464c14fcd130d9a526dd1fdd68bb
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-10-23 17:09:41 +0400

    Update matrix.py

--- 29.09.22/matrix.py
+++ 29.09.22/matrix.py
@@ -1,13 +1,20 @@
-rows = int(input("Give the number of rows: "))  
-columns = int(input("Give the number of columns: "))  
-matrix = []  
-for i in range(rows): 
-    r = []  
-    for j in range(columns):   
-        r.append(int(input("Enter the rows and columns: ")))  
-    matrix.append(r)  
- 
-for i in range(rows):  
-    for j in range(columns):  
-        print(matrix[i][j], end=" ")  
-    print()  
+n = 5
+matrix = [[int(input("Enter a number: ")) for i in range(n)] for j in range(n)]
+print(matrix)
+dict_ = {}
+key = dict_.keys()
+for i in matrix:
+    for j in i:
+        if j in key:
+            dict_[j] += 1
+        else:
+            dict_.update({j:1})
+print(dict_)
+res = []
+x = list(dict_.values())
+x.sort()
+x = x[-3:]
+for i in x:
+    for j in dict_.keys():
+        if dict_[j] == i:
+            print(j)
